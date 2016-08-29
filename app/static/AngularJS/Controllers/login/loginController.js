94b7051c8e4a862715eb236c51218efda7c0daad
Edwin CRUZ <edwincrug@gmail.com>
2016-08-29 13:17:42 -0500

    Login

--- app/static/AngularJS/Controllers/login/loginController.js
+++ app/static/AngularJS/Controllers/login/loginController.js
@@ -1,4 +1,4 @@
-appControllers.controller('loginController', function ($scope, loginRepository,notificationFactory) {
+﻿appControllers.controller('loginController', function ($scope, loginRepository,notificationFactory, localStorageService) {
     //this is the first method executed in the view
     $scope.init = function () {        
         cargaDatosUsuario();
@@ -7,18 +7,20 @@ appControllers.controller('loginController', function ($scope, loginRepository,n
     //Obtiene al usuario hardcodeado
     var cargaDatosUsuario = function () {
         if (!($('#lgnUser').val().indexOf('[') > -1)) {
-                localStorageService.set('lgnUser', $('#lgnUser').val());
+                 localStorageService.set('lgnUser', $('#lgnUser').val());
+		$scope.idUsuario = localStorageService.get('lgnUser')
             } else {
                 if (($('#lgnUser').val().indexOf('[') > -1) && !localStorageService.get('lgnUser')) {
                     if (getParameterByName('employee') != '') {
                         $rootScope.currentEmployee = getParameterByName('employee');
+			console.log('paso aqui')
                     } else {
                         alert('Inicie sesión desde panel de aplicaciones.');
                         window.close(); 
                     }
                 }
             }
-        loginRepository.cargaDatosUsuario(10)
+        loginRepository.cargaDatosUsuario($scope.idUsuario)
             .then(
                 function succesCallback(response) {
                     //Success
